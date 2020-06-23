c140e45f2963d67aaa2ff1bf243416b26266d2a7
Mridul Gupta <mrgupta@microsoft.com>
2020-06-23 14:38:24 +0530

    update

--- src/components/appBody/tree/tree.js
+++ src/components/appBody/tree/tree.js
@@ -3,7 +3,7 @@ import Level from './level';
 import { getDataById } from '../../../utilities/mockApi';
 
 const Tree = () => {
-    const data = getDataById();
+    const data = getDataById(5);
     console.log(data);
 
     return (
--- src/utilities/mockApi.js
+++ src/utilities/mockApi.js
@@ -7,15 +7,23 @@ export const getDataById = (id = 1) => {
     const spouse = totalData.find(x => id === x.spouse);
     const father = totalData.find(x => x.id === person.parents.father);
     const mother = totalData.find(x => x.id === person.parents.mother);
+
+    const paternalGrandparents = father ? totalData.filter(x => x.id === father.parents.father || x.id === father.parents.mother) : [[]];
+
     const personSiblings = father ? totalData.filter(x => father.children.includes(x.id)).map(x => {
         if (x.id === id) {
-            return [{ ...x, isCurrent: true}, spouse];
+            return [{ ...x, isCurrent: true }, spouse];
         }
 
-        return [x];
+        return [x, x.spouse ? totalData.find(y => y.id === x.spouse) : null];
     }) : [[person, spouse]];
-    const paternalGrandparents = father ? totalData.filter(x => x.id === father.parents.father || x.id === father.parents.mother) : [[]];
-    const children = person.children.length > 0 ? totalData.filter(x => person.children.includes(x.id)).map(x => [x]) : [[]];
+    
+    const children = person.children.length > 0 ?
+        totalData
+            .filter(x => person.children.includes(x.id))
+            .map(x => [x, x.spouse ? totalData.find(y => y.id === x.spouse) : null])
+        :
+        [[]];
 
     return [
         [paternalGrandparents],
